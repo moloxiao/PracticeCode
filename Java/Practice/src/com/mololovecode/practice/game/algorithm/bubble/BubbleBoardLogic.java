a7035c073f5a27bacc6409080fddfe726750df4f
moloxiao <molo.xiao@gmail.com>
2014-11-04 15:34:59 +0800

    泡泡龙逻辑-掉落检查

--- Java/Practice/src/com/mololovecode/practice/game/algorithm/bubble/BubbleBoardLogic.java
+++ Java/Practice/src/com/mololovecode/practice/game/algorithm/bubble/BubbleBoardLogic.java
@@ -37,10 +37,17 @@ public class BubbleBoardLogic {
 				}
 			}
 		}
+		board = checkDrop(board, firstLineRetract);
 		
 		return board;
 	}
 	
+	/**
+	 * 检查有哪些元素是何当前位置元素连接在一起的
+	 * @param position 元素位置
+	 * @param board 元素所在的棋盘
+	 * @param firstLineRetract 第一行是否缩进
+	 */
 	public static void checkLine(int position, int[] board, final boolean firstLineRetract) {
 		if(!inBoard(position)) {
 			return;
@@ -128,6 +135,119 @@ public class BubbleBoardLogic {
 			}
 		}
 		
+	}
+	
+	/**
+	 * 检查是否有泡泡掉落。
+	 * @param board
+	 * @param firstLineRetract
+	 * @return
+	 */
+	public static int[] checkDrop(int[] board, final boolean firstLineRetract) {
+		zeroBoard = null;
+		zeroBoard = BubbleBoardLogic.getZeroBoard();
+		zeroCount = 0;
+		for(int i=0;i<BubbleBoardLogic.BOARD_X_NUMBER;i++) {
+			if( board[i] != 0 && zeroBoard[i]==0 ) {
+				checkConnect(i, board, firstLineRetract);
+			}
+		}
+			
+		for(int i=0;i<BOARD_NUMBER;i++) {
+			if(zeroBoard[i] == 0) {
+				board[i] = 0;
+			}
+		}
+		
+		return board;
+	}
+	
+	public static void checkConnect(int position, int[] board, final boolean firstLineRetract) {
+		if(!inBoard(position)) {
+			return;
+		}else {
+			zeroBoard[position] = 1;
+			zeroCount++;
+		}
+		
+		int x = BubbleBoardLogic.getXByPosition(position);
+		int y = BubbleBoardLogic.getYByPosition(position);
+		boolean needRetract = y%2==0?firstLineRetract:!firstLineRetract;
+		
+		// step1 : checkLeft
+		if(x > 0) {
+			int leftPosition = BubbleBoardLogic.getPositionByXAndY(x-1, y);
+			if(board[leftPosition] != 0 &&
+					zeroBoard[leftPosition] == 0) {
+				checkConnect( leftPosition, board, firstLineRetract );
+			}
+		}
+		
+		// step2 : checkRight
+		if( x < BubbleBoardLogic.BOARD_X_NUMBER-1 ) {
+			int rightPosition = BubbleBoardLogic.getPositionByXAndY(x+1, y);
+			if(board[rightPosition] != 0 &&
+					zeroBoard[rightPosition] == 0) {
+				checkConnect( rightPosition, board, firstLineRetract );
+			}
+		}
+		
+		// step3 : checkTop
+		if(y > 0) {
+			int topPosition = BubbleBoardLogic.getPositionByXAndY(x, y-1);
+			if(board[topPosition] == board[position] &&
+					zeroBoard[topPosition] == 0) {
+				checkConnect( topPosition, board, firstLineRetract );
+			}
+		}
+		
+		// step4 : checkTopNext
+		if(needRetract) {
+			if(y > 0 && x < BubbleBoardLogic.BOARD_X_NUMBER-1) {
+				int topNextPosition = BubbleBoardLogic.getPositionByXAndY(x+1, y-1);
+				if(board[topNextPosition] != 0 &&
+						zeroBoard[topNextPosition] == 0) {
+					checkConnect( topNextPosition, board, firstLineRetract );
+				}
+			}
+		}else {
+			if(y > 0 && x > 0) {
+				int topNextPosition = BubbleBoardLogic.getPositionByXAndY(x-1, y-1);
+				if(board[topNextPosition] != 0 &&
+						zeroBoard[topNextPosition] == 0) {
+					checkConnect( topNextPosition, board, firstLineRetract );
+				}
+			}
+		}
+		
+		
+		// step5 : checkBottom
+		if(y < BubbleBoardLogic.BOARD_Y_NUMBER-1) {
+			int bottomPosition = BubbleBoardLogic.getPositionByXAndY(x, y+1);
+			if(board[bottomPosition] != 0 &&
+					zeroBoard[bottomPosition] == 0) {
+				checkConnect( bottomPosition, board, firstLineRetract );
+			}
+		}
+		
+		// step6 : checkBottomNext
+		if(needRetract) {
+			if(y < BubbleBoardLogic.BOARD_Y_NUMBER-1 && x < BubbleBoardLogic.BOARD_X_NUMBER-1) {
+				int bottomNextPosition = BubbleBoardLogic.getPositionByXAndY(x+1, y+1);
+				if(board[bottomNextPosition] != 0 &&
+						zeroBoard[bottomNextPosition] == 0) {
+					checkConnect( bottomNextPosition, board, firstLineRetract );
+				}
+			}
+		}else {
+			if(y < BubbleBoardLogic.BOARD_Y_NUMBER-1 && x > 0) {
+				int bottomNextPosition = BubbleBoardLogic.getPositionByXAndY(x-1, y+1);
+				if(board[bottomNextPosition] != 0 &&
+						zeroBoard[bottomNextPosition] == 0) {
+					checkConnect( bottomNextPosition, board, firstLineRetract );
+				}
+			}
+		}
 	}
 	
 	public static int getPositionByXAndY(int x, int y) {
